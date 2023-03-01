2cce85f73de8f99491949a4d6090c6a680d70e1e
callum1206 <126166741+callum1206@users.noreply.github.com>
2023-03-01 09:58:21 +0000

    Delete testing.py

--- stem saleries/testing.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv(r"C:\Users\callu\Documents\PYTHON PROJECTS\pandas\stem saleries\STEM_saleries.csv") 
-
-#converting the timestamp column to a datetime object
-df['timestamp'] = pd.to_datetime(df['timestamp'])
-
-print(df['title'].value_counts())
